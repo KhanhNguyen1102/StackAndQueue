3b03e3e5302dbec4c43625cc0e8f89b1fe64eae0
KhanhNguyen1102 <sukhanh12@gmail.com>
2021-10-18 23:03:12 +0700

    update

--- src/ReverseArrayWithStack/ReverseArrayWithStack.java
+++ src/ReverseArrayWithStack/ReverseArrayWithStack.java
@@ -4,28 +4,30 @@ import java.util.Stack;
 
 public class ReverseArrayWithStack {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>();
+
+//        Stack<String> stack = new Stack<>();
+//        String[] arr = str.split("");
+//        for (String s : arr) {
+//            stack.push(s);
+//        }
+//        Stack<String> stack2 = new Stack<>();
+//        while (!stack.isEmpty()) {
+//            stack2.push(stack.pop());
+//        }
+//        System.out.println(stack2);
         String str = "123456";
-        String[] arr = str.split("");
-        for (String s : arr) {
-            stack.push(s);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
         }
-        Stack<String> stack2 = new Stack<>();
-        while (!stack.isEmpty()) {
-            stack2.push(stack.pop());
+        char[] characters = new char[stack.size()];
+        for (int i = 0; i < str.length(); i++) {
+            characters[i]=stack.pop();
         }
-        System.out.println(stack2);
+        String str2 = new String(characters);
+        System.out.println(str2);
     }
 }
-            // Add elements to Stack
-//            animals.push("Dog");
-//            animals.push("Horse");
-//            animals.push("Cat");
-//            System.out.println(animals.peek());
-//            System.out.println("Stack: " + animals);
-//            for (String animal:animals) {
-//                System.out.println(animal);
-//            }
 
 
 
